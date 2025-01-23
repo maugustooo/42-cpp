ac432ed40067ca883a3f3675dd60cefed467c52b
maugusto <marcoamorim911@gmail.com>
2025-01-23 15:57:48 +0000

    mod6 stuff

--- module06/ex00/Scalar.hpp
+++ module06/ex00/Scalar.hpp
@@ -1,15 +1,33 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <iomanip> 
+
+#define CHAR 1
+#define INT 2
+#define FLOAT 3
+#define DOUBLE 4
 
 class ScalarConverter
 {
 private:
 	std::string _input;
+	int 		_type;
+	ScalarConverter();
+	static int HandleInput(const std::string input);
+	int GetType();
+	std::string GetInput();
 public:
 	ScalarConverter();
 	ScalarConverter(const std::string &input);
 	ScalarConverter(const ScalarConverter &scalar);
 	ScalarConverter &operator=(ScalarConverter const &rSym);
 	~ScalarConverter();
+
+	static void convert(const std::string input);
 };
+
+void convertChar(const std::string input);
+void convertInt(const std::string input);
+void convertFloat(const std::string input);
+void convertDouble(const std::string input);
